1f978cd4f5d32b283b4e893bb05aa2215fc699d3
RAUSHANPKUMAR <65502377+RAUSHANPKUMAR@users.noreply.github.com>
2020-09-30 22:55:46 +0530

    Update app.js

--- task list/app.js
+++ task list/app.js
@@ -59,6 +59,21 @@ function addTask(e) {
   if(taskInput.value === '') {
     alert('Add a task');
   }
+ // Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+ // DOM Load event
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // DOM Load event
+  document.addEventListener('DOMContentLoaded', getTasks);
 
   // Create li element
   const li = document.createElement('li');
